7800c6c223dc40b96db112e0f8bf35e78233ecd5
Chihiro Shibayama <chihiro.shibayama@gmail.com>
2023-01-27 21:18:05 -0800

    fixed card's image size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,15 +39,11 @@ export default function Home() {
           href='/favicon.ico'
         />
       </Head>
-      {/* <Flex
-        height='100vh'
-        alignItems='center'
-        flexDirection='column'
-        justifyContent='center'
-      > */}
+
       <SimpleGrid
         spacing={4}
-        templateColumns='repeat(auto-fill, minmax(300px, 1fr))'
+        minChildWidth='300px'
+        // templateColumns='repeat(auto-fill, minmax(300px, 1fr))'
       >
         <Card
           overflow='hidden'
@@ -64,13 +60,13 @@ export default function Home() {
             </Heading>
             <Image
               objectFit='cover'
-              maxW={{ base: '100%', sm: '300px' }}
-              src='https://github.com/oslabs-beta/RediScope/raw/main/src/components/images/Rediscope-banner.png'
-              alt='RediScope Logo'
+              maxW={{ base: '100%' }}
+              src='https://github.com/oslabs-beta/RediScope/raw/main/src/components/images/giphygraph.gif'
+              alt='RediScope Graph GIF'
               borderRadius='lg'
             />
             <Text py='2'>
-              open-source web application that intuitively and elegantly displays relevant Redis cache performance metrics in real-time
+              An open-source web application that intuitively and elegantly displays relevant Redis cache performance metrics in real-time
             </Text>
           </CardBody>
 
@@ -98,13 +94,46 @@ export default function Home() {
             </Heading>
             <Image
               objectFit='cover'
-              maxW={{ base: '100%', sm: '300px' }}
-              src='https://github.com/oslabs-beta/RediScope/raw/main/src/components/images/Rediscope-banner.png'
-              alt='RediScope Logo'
+              maxW={{ base: '100%' }}
+              src='https://github.com/cheesehero112/Places-You-ll-Go/raw/main/media/Places-Screenshot1.png'
+              alt="Places You'll Go screenshot"
+              borderRadius='lg'
+            />
+            <Text py='2'>A web application to keep track of dream destinations and visualize them on a map using ArcGIS API</Text>
+          </CardBody>
+
+          <CardFooter>
+            <Button
+              variant='solid'
+              colorScheme='blue'
+            >
+              Learn more
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card
+          overflow='hidden'
+          variant='outline'
+          maxW='md'
+          m={4}
+        >
+          <CardBody>
+            <Heading
+              size='lg'
+              mb={3}
+            >
+              Places You&apos;ll Go
+            </Heading>
+            <Image
+              objectFit='cover'
+              maxW={{ base: '100%' }}
+              src='https://github.com/cheesehero112/Localization-Example/raw/main/media/Localization-Jp.png'
+              alt='Localization Example Screenshot'
               borderRadius='lg'
             />
             <Text py='2'>
-              open-source web application that intuitively and elegantly displays relevant Redis cache performance metrics in real-time
+              a simple example of how to localize a single website into multiple languages. A user can select between English, Spanish, and Japanese
+              by clicking a corresponding button
             </Text>
           </CardBody>
 
