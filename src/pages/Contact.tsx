27303d897659c11d588cb719adfc74e81e5809ec
Chihiro Shibayama <chihiro.shibayama@gmail.com>
2023-01-29 12:05:54 -0800

    added contact pages and projects

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,10 @@
+import { Heading, Box } from '@chakra-ui/react';
+
+export default function Contact() {
+  return (
+    <>
+      <Heading size='lg'>Contact</Heading>
+      <Box></Box>
+    </>
+  );
+}
---
+++ src/pages/Project.tsx
@@ -0,0 +1,11 @@
+import { Projects } from './components/Projects';
+import { Heading } from '@chakra-ui/react';
+
+export default function Project() {
+  return (
+    <>
+      <Heading>Projects</Heading>
+      <Projects />
+    </>
+  );
+}
