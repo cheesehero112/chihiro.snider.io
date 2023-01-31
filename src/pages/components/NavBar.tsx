8e07cc0e61b05367ca43753cb3463fe1b24e2d4d
Chihiro Shibayama <chihiro.shibayama@gmail.com>
2023-01-30 20:10:34 -0800

    connected the nav bar and pages link

--- src/pages/components/NavBar.tsx
+++ src/pages/components/NavBar.tsx
@@ -29,7 +29,7 @@ export const NavLink = ({ children }: { children: ReactNode }) => (
       textDecoration: 'none',
       bg: useColorModeValue('gray.200', 'gray.700'),
     }}
-    href={'#'}
+    href={`${children}`}
   >
     {children}
   </Link>
