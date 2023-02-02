36d3ac1de45446f5c0a3cfbdda101489a6a5977c
Chihiro Shibayama <chihiro.shibayama@gmail.com>
2023-02-01 16:59:48 -0800

    contact page added

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,52 @@
-import { Heading, Box } from '@chakra-ui/react';
+import { Heading, Box, Image, Text, List, ListItem, ListIcon, Stack } from '@chakra-ui/react';
 
 export default function Contact() {
   return (
-    <>
-      <Heading size='lg'>Contact</Heading>
-      <Box></Box>
-    </>
+    <Stack
+      display='flex'
+      flex-flexDirection='column'
+      m={8}
+    >
+      <Heading
+        size='lg'
+        m={8}
+      >
+        Contact
+      </Heading>
+
+      <Stack
+        display='flex'
+        flexDirection='row'
+      >
+        <Image
+          borderRadius='full'
+          boxSize='200px'
+          minW='200px'
+          src='https://github.com/oslabs-beta/RediScope/raw/main/src/components/images/Chihiroheadshot.png'
+          alt='Chihiro Snider'
+          m={6}
+        />
+        <Box
+          display='flex'
+          alignItems='center'
+          width='100%'
+        >
+          <List spacing={6}>
+            <ListItem>
+              <ListIcon />
+              Email
+            </ListItem>
+            <ListItem>
+              <ListIcon />
+              GitHub
+            </ListItem>
+            <ListItem>
+              <ListIcon />
+              LinkedIn
+            </ListItem>
+          </List>
+        </Box>
+      </Stack>
+    </Stack>
   );
 }
