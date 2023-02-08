e9cde8029e5baccdb0a0f8e4976e7cfaf0d9e258
Chihiro Shibayama <chihiro.shibayama@gmail.com>
2023-02-07 16:12:12 -0800

    new favicons added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,9 +21,27 @@ export default function Home() {
           name='viewport'
           content='width=device-width, initial-scale=1'
         />
+
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
         <link
           rel='icon'
-          href='https://github.com/cheesehero112/chihiro.snider.io/raw/main/public/fav-portfolio.png'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link
+          rel='manifest'
+          href='/site.webmanifest'
         />
       </Head>
       <About />
